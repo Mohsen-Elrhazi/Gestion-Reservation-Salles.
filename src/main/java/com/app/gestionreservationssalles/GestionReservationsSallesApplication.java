474a50be14c27525c184d101163f3110f569f566
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2025-12-16 13:52:16 +0100

    Ãfirst commit

---
+++ src/main/java/com/app/gestionreservationssalles/GestionReservationsSallesApplication.java
@@ -0,0 +1,13 @@
+package com.app.gestionreservationssalles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionReservationsSallesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionReservationsSallesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/gestionreservationssalles/GestionReservationsSallesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.gestionreservationssalles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionReservationsSallesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
