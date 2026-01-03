3c9e7ba121fe77d17b9286086bb2414560ab8eea
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2026-01-03 21:29:57 +0100

    Créer l'interface UserService

---
+++ src/main/java/com/app/gestionreservationssalles/service/UserService.java
@@ -0,0 +1,20 @@
+package com.app.gestionreservationssalles.service;
+
+import com.app.gestionreservationssalles.dto.request.UserRequestDTO;
+import com.app.gestionreservationssalles.dto.request.UserUpdateDTO;
+import com.app.gestionreservationssalles.dto.response.UserResponseDTO;
+
+import java.util.List;
+
+public interface UserService {
+    // CRUD de base
+    UserResponseDTO createUser(UserRequestDTO dto);
+    UserResponseDTO updateUser(Long id, UserUpdateDTO dto);
+
+    // Activer / Désactiver
+    UserResponseDTO basculerStatut(Long id);
+
+    // Lire
+    UserResponseDTO getUserById(Long id);
+    List<UserResponseDTO> getAllUsers();
+}
